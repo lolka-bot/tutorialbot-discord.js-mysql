aba8571e5ffd8f4dfb4a52a287f6c4a06899ef97
lolka <39336434+lolka-bot@users.noreply.github.com>
2021-01-19 21:36:39 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,33 +1,61 @@
 const Discord = require("discord.js"); // Подключаем модуль D.JS: Установка npm install discord.js
+
 const bot = new Discord.Client(); // Создаем нового клиента
+
+bot.commands = new Discord.Collection(); // для работы обработчика команд
+
 let config = require("./botconfig.json"); // Подключаем файл с настройкой бота
+
 let token = config.token; // Ищем в файле botconfig.json токен
+
 let prefix = config.prefix; // Ищем в файле botconfig.json префикс
+
 var mysql = require('mysql'); // Подключаем модуль mysql: Установка: npm install mysql
 // Делаем данные для подключения
+
+// Command handler (Обработчик команд)
+fs.readdir('./commands/',(err,files)=>{
+    if(err) console.log(err);
+    let jsfiles = files.filter(f => f.split(".").pop() === "js");
+    if(jsfiles.length <=0) console.log("Нет комманд для загрузки!!");
+    console.log(`Загружено ${jsfiles.length} комманд`);
+    jsfiles.forEach((f,i) =>{
+        let props = require(`./cmds/${f}`);
+        console.log(`${i+1}.${f} Загружен!`);
+        bot.commands.set(props.help.name,props);
+    });
+});
+
 var con = mysql.createConnection({
   host:'127.0.0.1',
   user:'root',
   password:'root',
   database:'testdb'
 })
+
 // Создаем новое подключение
+
 con.connect(err => {
   if(err) throw err;
   console.log("[DB] Подключение успешно!")
 })
+
 // Делаем после запуска:
 bot.on("ready", () => {
+
   console.log(`Запустился бот ${bot.user.tag}!`);
-  //Создаем приглашение и отправляем его в консоль
+  
+  //Создаем приглашение и отправляем его в консоль 
   bot.generateInvite(["ADMINISTRATOR"]).then(link => {
      console.log(link);
   });
 });
+
 // Если бота добавили на сервер:
 bot.on("guildCreate", async (guild) => {
   // делаем запрос на проверку сервера: gid - id сервера на который бот вступил
   con.query(`SELECT * FROM guilds WHERE gid = '${guild.id}'`, async (err, result) => {
+    
     if(err) throw err;
     // Если нет сервера:
     if(result.length < 1 || !result) {
@@ -38,9 +66,11 @@ bot.on("guildCreate", async (guild) => {
   })
 })
 bot.on("guildDelete", async (guild) => {
-   if (err) throw err;
+   
+  if (err) throw err;
   // Здесь аналогично как и при добавлении на сервер, только мы удаляем из бд
-   con.query(`SELECT * FROM guilds WHERE gid = '${guild.id}'`, async (err, result) => {
+  
+  con.query(`SELECT * FROM guilds WHERE gid = '${guild.id}'`, async (err, result) => {
       if(err) throw err;
       if(result.length < 1){
         return;
@@ -53,32 +83,26 @@ bot.on("guildDelete", async (guild) => {
 bot.on("message", async message => {
   if(message.author.bot) return; // Если автор сообщения бот, ничего не делаем
   if(message.channel.type === dm) return; // Если сообщение в лс, то ничего не делаем
+  
   // Просматриваем таблицу в бд
+  
   con.query(`SELECT * FROM users WHERE id = '${message.author.id}'`, async (err, result) => {
+    
      if(err) throw err;
     // Если пользователя нет в бд, то добавляем
+    
      if(result.length < 1 || !result){
        con.query(`INSERT INTO users (id) VALUES ('${message.author.id}')`) // id это id пользователя который отправил сообщение
      } else {
      return; // Если пользователь есть, ничего не делаем
      }
   })
-  if (message.content === `${prefix}ping`) {
-    message.channel.send('Pong!');
-  }
-  if (message.content === `${prefix}examplembed`) {
-      let embed = new Discord.MessageEmbed()
-        .setTitle(`Название`)
-        .setDescription(`Описание`)
-        .setColor(`Цвет в HEX`)
-        .setAuthor(`Автор`)
-        .setUrl(`Здесь ссылка`)
-        .addField(`Название поля`, `Содержание поля`)
-        .setThumbnail(message.author.displayAvatarURL()) // Аватарка
-        .setTimestamp() // Указываем время
-        .setFooter(`Подпись снизу`, `Аватарка`)
-      message.channel.send(embed)
-  }
+    let messageArray = message.content.split(" "); // содержкание сообщения
+    let command = messageArray[0].toLowerCase(); // для того, чтобы если сообщение написано верхнем регистром пример: !HELP, бот переводит его в нижний !help
+    let args = messageArray.slice(1); 
+    if(!message.content.startsWith(prefix)) return; // если сообщений не начинается с префикса, то ничего не делаем
+    let cmd = bot.commands.get(command.slice(prefix.length));
+    if(cmd) cmd.run(bot,message,args);
 });
 
 bot.login(token); // Авторизовываем бота
